25e8fcbc4f76dafb3fb061389d0cbab80aa9bca1
IhsanFauzan98 <114044976+IhsanFauzan98@users.noreply.github.com>
2023-12-07 19:31:08 +0800

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:kelompok_c2/main.dart';
+import 'package:aplikasi_kfc/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
